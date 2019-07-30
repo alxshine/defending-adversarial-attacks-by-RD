c8531d2a408419873941f5138c821550e440ae03
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2019-07-30 13:52:04 +0200

    Cleaned up MNIST setup

--- libs/setup_mnist.py
+++ libs/setup_mnist.py
@@ -1,125 +1,45 @@
-## setup_mnist.py -- mnist data and model loading code
-##
-## Copyright (C) 2016, Nicholas Carlini <nicholas@carlini.com>.
-##
-## This program is licenced under the BSD 2-Clause licence,
-## contained in the LICENCE file in this directory.
+""" setup_mnist.py -- mnist data and model loading code
+
+ Copyright (C) 2016, Nicholas Carlini <nicholas@carlini.com>.
+
+ This program is licenced under the BSD 2-Clause licence,
+ contained in the LICENCE file in this directory.
+"""
+
+import gzip
+import numpy as np
 
 import tensorflow as tf
-
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.optimizers import SGD, Adam
-from keras.utils import np_utils
-from keras.models import load_model
-import os
-import urllib
-import gzip
-import numpy as np
+
 
 def extract_data(filename, num_images):
+    """ extract image data from .tar.gz archive """
+
     with gzip.open(filename) as bytestream:
         bytestream.read(16)
-        buf = bytestream.read(num_images*28*28)
+        buf = bytestream.read(num_images * 28 * 28)
         data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
         data = (data / 255) - 0.5
         data = data.reshape(num_images, 28, 28, 1)
         return data
 
+
 def extract_labels(filename, num_images):
+    """ extract labels from .tar.gz archive """
     with gzip.open(filename) as bytestream:
         bytestream.read(8)
         buf = bytestream.read(1 * num_images)
         labels = np.frombuffer(buf, dtype=np.uint8)
     return (np.arange(10) == labels[:, None]).astype(np.float32)
 
-class MNIST:
-    def __init__(self):
-        if not os.path.exists("data"):
-            os.mkdir("data")
-            files = ["train-images-idx3-ubyte.gz",
-                     "t10k-images-idx3-ubyte.gz",
-                     "train-labels-idx1-ubyte.gz",
-                     "t10k-labels-idx1-ubyte.gz"]
-            for name in files:
-
-                urllib.request.urlretrieve('http://yann.lecun.com/exdb/mnist/' + name, "data/"+name)
-
-        train_data = extract_data("data/train-images-idx3-ubyte.gz", 60000)
-        train_labels = extract_labels("data/train-labels-idx1-ubyte.gz", 60000)
-        self.test_data = extract_data("data/t10k-images-idx3-ubyte.gz", 10000)
-        self.test_labels = extract_labels("data/t10k-labels-idx1-ubyte.gz", 10000)
-        
-        VALIDATION_SIZE = 5000
-        
-        self.validation_data = train_data[:VALIDATION_SIZE, :, :, :]
-        self.validation_labels = train_labels[:VALIDATION_SIZE]
-        self.train_data = train_data[VALIDATION_SIZE:, :, :, :]
-        self.train_labels = train_labels[VALIDATION_SIZE:]
-
-class FashionMNIST:
-    def __init__(self):
-        if not os.path.exists("data/fashion-mnist"):
-            os.mkdir("data/fashion-mnist")
-            files = ["train-images-idx3-ubyte.gz",
-                     "t10k-images-idx3-ubyte.gz",
-                     "train-labels-idx1-ubyte.gz",
-                     "t10k-labels-idx1-ubyte.gz"]
-            for name in files:
-
-                urllib.request.urlretrieve('http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/' + name, "data/fashion-mnist/"+name)
-
-        train_data = extract_data("data/fashion-mnist/train-images-idx3-ubyte.gz", 60000)
-        train_labels = extract_labels("data/fashion-mnist/train-labels-idx1-ubyte.gz", 60000)
-        self.test_data = extract_data("data/fashion-mnist/t10k-images-idx3-ubyte.gz", 10000)
-        self.test_labels = extract_labels("data/fashion-mnist/t10k-labels-idx1-ubyte.gz", 10000)
-        
-        VALIDATION_SIZE = 5000
-        
-        self.validation_data = train_data[:VALIDATION_SIZE, :, :, :]
-        self.validation_labels = train_labels[:VALIDATION_SIZE]
-        self.train_data = train_data[VALIDATION_SIZE:, :, :, :]
-        self.train_labels = train_labels[VALIDATION_SIZE:]
-
-class MNISTModel:
-    def __init__(self, restore, session=None):
-        self.num_channels = 1
-        self.image_size = 28
-        self.num_labels = 10
-
-        model = Sequential()
-
-        model.add(Conv2D(32, (3, 3),
-                         input_shape=(28, 28, 1)))
-        model.add(Activation('relu'))
-        model.add(Conv2D(32, (3, 3)))
-        model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        
-        model.add(Conv2D(64, (3, 3)))
-        model.add(Activation('relu'))
-        model.add(Conv2D(64, (3, 3)))
-        model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        
-        model.add(Flatten())
-        model.add(Dense(200))
-        model.add(Activation('relu'))
-        model.add(Dense(200))
-        model.add(Activation('relu'))
-        model.add(Dense(10))
-        model.load_weights(restore)
-
-        self.model = model
-
-    def predict(self, data):
-        return self.model(data)
-
 
 class MNISTModelAllLayers:
-    def __init__(self, params=[32, 32, 64, 64, 200, 200], init=None, session=None):
-
+    """ Single MNIST channel """
+    def __init__(self, layer_sizes=None, init=None):
         self.train_temp = 1
 
         self.num_channels = 1
@@ -128,58 +48,74 @@ class MNISTModelAllLayers:
 
         model = Sequential()
 
-        model.add(Conv2D(params[0], (3, 3),
-                         input_shape=(28, 28, 1)))
+        if layer_sizes is None:
+            layer_sizes = [32, 32, 64, 64, 200, 200]
+
+        model.add(Conv2D(layer_sizes[0], (3, 3), input_shape=(28, 28, 1)))
         model.add(Activation('relu'))
-        model.add(Conv2D(params[1], (3, 3)))
+        model.add(Conv2D(layer_sizes[1], (3, 3)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
-        model.add(Conv2D(params[2], (3, 3)))
+        model.add(Conv2D(layer_sizes[2], (3, 3)))
         model.add(Activation('relu'))
-        model.add(Conv2D(params[3], (3, 3)))
+        model.add(Conv2D(layer_sizes[3], (3, 3)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
         model.add(Flatten())
-        model.add(Dense(params[4]))
+        model.add(Dense(layer_sizes[4]))
         model.add(Activation('relu'))
         model.add(Dropout(0.5))
-        model.add(Dense(params[5]))
+        model.add(Dense(layer_sizes[5]))
         model.add(Activation('relu'))
         model.add(Dense(10))
 
-        if init != None:
+        if init is not None:
             model.load_weights(init)
 
         self.model = model
 
-    def fn(self, correct, predicted):
-        return tf.nn.softmax_cross_entropy_with_logits(labels=correct,
-                                                       logits=predicted / self.train_temp)
+    def loss_function(self, correct, predicted):
+        """ loss function using softmax cross-entropy"""
+        return tf.nn.softmax_cross_entropy_with_logits(logits=predicted /
+                                                       self.train_temp,
+                                                       labels=correct)
 
-    def train(self, data, file, learning_rate=1e-3, batch_size=128, num_epochs=100, oprimazer="sgd"):
+    def train(self, data, file, train_params):
+        """ train using supplied parameters
+        Arguments:
+        train_params (dict) : dictionary containing
+            ['learning_rate', 'optimizer', 'batch_size', 'num_epochs'],
+            which all correspond to keras traning parameters
+ """
 
-        if oprimazer == "sgd":
-            opt = SGD(lr=learning_rate, decay=1e-6, momentum=0.9, nesterov=True)
-        elif oprimazer == "adam":
-            opt = Adam(lr=learning_rate)
+        if train_params['optimizer'] == "sgd":
+            opt = SGD(lr=train_params['learning_rate'],
+                      decay=1e-6,
+                      momentum=0.9,
+                      nesterov=True)
+        elif train_params['optimizer'] == "adam":
+            opt = Adam(lr=train_params['learning_rate'])
 
-        self.model.compile(loss=self.fn,
-                      optimizer=opt,
-                      metrics=['accuracy'])
+        self.model.compile(loss=self.loss_function,
+                           optimizer=opt,
+                           metrics=['accuracy'])
 
-        self.model.fit(data.train_data, data.train_labels,
-                  batch_size=batch_size,
-                  validation_data=(data.validation_data, data.validation_labels),
-                  epochs=num_epochs,
-                  verbose=2,
-                  shuffle=True)
+        self.model.fit(data.train_data,
+                       data.train_labels,
+                       batch_size=train_params['batch_size'],
+                       validation_data=(data.validation_data,
+                                        data.validation_labels),
+                       epochs=train_params['num_epochs'],
+                       verbose=2,
+                       shuffle=True)
 
-        if file != None:
+        if file is not None:
             self.model.save(file)
 
         return self.model
 
     def predict(self, data):
+        """ return prediction results of underlying model """
         return self.model(data)
