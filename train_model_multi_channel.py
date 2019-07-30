9a61c3325f75441d6d04a028692af686d597cced
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2019-07-30 11:41:05 +0200

    Cleaned up train script

--- train_model_multi_channel.py
+++ train_model_multi_channel.py
@@ -1,95 +1,111 @@
 """Defending against adversarial attacks by randomized diversification"""
 
 import argparse
-import keras
 import os
+import keras
+import tensorflow as tf
 
-from setup_mnist import *
-from setup_cifar import *
+from libs.setup_mnist import MNIST, MNISTModelAllLayers, FashionMNIST
+from libs.setup_cifar import CIFAR, CIFARModelAllLayers
 import libs.model_multi_channel as mcm
 
-########################################################################################################################
+#######################################################
 
-def makeDir(dir):
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-    return dir
 
-########################################################################################################################
-parser = argparse.ArgumentParser(description="Train multi-channel system with randomized diversification.")
-parser.add_argument("--type", default="mnist", help="The dataset.")
-parser.add_argument("--save_to", default="models", help="Path where to save models.")
-parser.add_argument("--is_zero", default=False, type=int, help="Is to use hard thresholding.")
-parser.add_argument("--epochs", default=50, type=int, help="The number of epochs.")
-parser.add_argument("--lr", default=1e-3, type=float, help="Learning rate.")
-parser.add_argument("--oprimazer", default="adam", help="The oprimazation technique.")
-parser.add_argument("--batch_size", default=64, type=int, help="Batch size.")
+def make_dir(dir_name):
+    """Create directory dir_name if it doesn't exist"""
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    return dir_name
 
-args = parser.parse_args()
 
-########################################################################################################################
-if __name__ == "__main__":
+#######################################################
 
-    P = [1, 2, 3]  # number of channels per subband
-    SUBBANDS = ["d", "h", "v"]  # DCT subbands
-    SAVE_TO_DIR = makeDir(args.save_to + "/" + args.type)
 
-    with tf.Session() as sess:
+#######################################################
+
+
+def train(model_type, epochs, optimizer, learning_rate, batch_size):
+    """ Train a multi-channel model
+
+    Args:
+        model_type (str) : the type of model to train,
+            one of: ['mnist', 'fashion_mnist', 'cifar']
+        epochs : the epochs to train for
+        optimizer : the keras optimizer to use,
+            see https://keras.io/optimizers/
+        learning_rate : the keras learning rate
+        batch_size : the keras batch size
+    """
+
+    channels_per_subband = [1, 2, 3]  # number of channels per subband
+    dct_subbands = ["d", "h", "v"]  # DCT subbands
+    model_save_dir = make_dir(os.path.join("models", model_type))
+
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+
+    with tf.Session(config=config) as sess:
         keras.backend.set_session(sess)
 
         # training data and parameters
-        if args.type == "mnist":
+        if model_type == "mnist":
             data = MNIST()
-            nn_param = [32, 32, 64, 64, 200, 200]
-            model = MNISTModelAllLayers(nn_param, session=sess)
-            IMAGE_SIZE = 28
-            N_CHANELS = 1
-        elif args.type == "fashion_mnist":
+            nn_layer_sizes = [32, 32, 64, 64, 200, 200]
+            model = MNISTModelAllLayers(nn_layer_sizes, session=sess)
+            image_size = 28
+            num_channels = 1
+        elif model_type == "fashion_mnist":
             data = FashionMNIST()
-            nn_param = [32, 32, 64, 64, 200, 200]
-            model = MNISTModelAllLayers(nn_param, session=sess)
-            IMAGE_SIZE = 28
-            N_CHANELS = 1
-        elif args.type == "cifar":
+            nn_layer_sizes = [32, 32, 64, 64, 200, 200]
+            model = MNISTModelAllLayers(nn_layer_sizes, session=sess)
+            image_size = 28
+            num_channels = 1
+        elif model_type == "cifar":
             data = CIFAR()
-            nn_param = [64, 64, 128, 128, 256, 256]
-            model = CIFARModelAllLayers(nn_param, session=sess)
-            IMAGE_SIZE = 32
-            N_CHANELS = 3
+            nn_layer_sizes = [64, 64, 128, 128, 256, 256]
+            model = CIFARModelAllLayers(nn_layer_sizes, session=sess)
+            image_size = 32
+            num_channels = 3
 
-        # multi-channel model initialization with classifier defined in model variable
+        # multi-channel model initialization with
+        # classifier defined in model variable
         multi_channel_model = mcm.MultiChannel(model,
-                                               type          = args.type,
-                                               epochs        = args.epochs,
-                                               optimazer     = args.oprimazer,
-                                               learning_rate = args.lr,
-                                               batch_size    = args.batch_size,
-                                               permt         = P,
-                                               subbands      = SUBBANDS,
-                                               model_dir     = SAVE_TO_DIR,
-                                               img_size      = IMAGE_SIZE,
-                                               img_channels  = N_CHANELS)
+                                               type=model_type,
+                                               epochs=epochs,
+                                               optimazer=optimizer,
+                                               learning_rate=learning_rate,
+                                               batch_size=batch_size,
+                                               permt=channels_per_subband,
+                                               subbands=dct_subbands,
+                                               model_dir=model_save_dir,
+                                               img_size=image_size,
+                                               img_channels=num_channels)
         # multi-channel model training
         multi_channel_model.train(data)
 
 
+def main():
+    parser = argparse.ArgumentParser(
+        description="Train multi-channel system with randomized diversification.")
+    parser.add_argument("--type", default="mnist", help="The dataset.")
+    parser.add_argument("--save_to", default="models",
+                        help="Path where to save models.")
+    parser.add_argument("--is_zero", default=False, type=int,
+                        help="Is to use hard thresholding.")
+    parser.add_argument("--epochs", default=50, type=int,
+                        help="The number of epochs.")
+    parser.add_argument("--lr", default=1e-3, type=float,
+                        help="Learning rate.")
+    parser.add_argument("--optimizer", default="adam",
+                        help="The oprimazation technique.")
+    parser.add_argument("--batch_size", default=64,
+                        type=int, help="Batch size.")
+
+    args = parser.parse_args()
+
+    train(args.type, args.epochs, args.optimizer, args.lr, args.batch_size)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
